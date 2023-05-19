96cb42c40b519442a02a43d039241cad1fb5346b
Lizzy <le016321@ohio.edu>
2023-05-19 13:45:08 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Elizabeth Ehleringer
+ *        Date: 5/19/2023
+ * Description: program that prints sentence
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
